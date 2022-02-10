54021e950b65a684f7ad14c8c0a5d2668e0fab9a
Ryosuke <piesuke0727@gmail.com>
2022-02-10 20:35:21 +0900

    fix: id周りを見直した

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,31 @@
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+import { FONT_COLOR, MAIN_COLOR } from "../../utils/const";
+import Footer from "../Footer";
+import Header from "../Header";
+import Wrapper from "../Wrapper";
+
+
+type Props = ({
+    children: ReactNode
+})
+
+const Layout: React.FC<Props> = ({
+    children
+}) => {
+   return(
+        <>
+            <Box background={MAIN_COLOR} color={FONT_COLOR}>
+                <Header />
+                <main className="main">
+                    <Wrapper>
+                        {children}
+                    </Wrapper>
+                </main>
+                <Footer />
+            </Box>
+        </>
+    )
+}
+
+export default Layout;
