2e33dde8ec302bc6f441f01e65341d115606cc88
Ryosuke <piesuke0727@gmail.com>
2022-02-10 21:59:24 +0900

    feat: highright.jsを導入

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -2,15 +2,14 @@ import { unified } from "unified";
 import remarkParse from 'remark-parse';
 import remark2rehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
-
-const shiki = require('rehype-shiki')
+import rehypeHighlight from 'rehype-highlight'
 
 const markdownToHtml = async(markdown: string) => {
     const result = await unified()
     .use(remarkParse)
     .use(remark2rehype)
-    .use(shiki, { theme: 'monokai' })
     .use(rehypeStringify)
+    .use(rehypeHighlight)
     .process(markdown)
 
     return result.toString();
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import 'highlight.js/styles/default.css';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
